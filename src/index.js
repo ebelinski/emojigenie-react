b0624110999fcf6e334d5d735c15239b918673ba
Eugene Belinski <ebelinski@protonmail.com>
2018-07-12 18:41:04 -0500

    created AppWithStore

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { reducer, filterMiddleware, actionTypes, initialState } from './redux';
+
+const store = createStore(reducer, initialState, applyMiddleware(filterMiddleware));
+
+const AppWithStore = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<AppWithStore />, document.getElementById('root'));
 registerServiceWorker();
