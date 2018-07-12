8f1173b6d07aad154b2cae159d30dbd8caa23174
Eugene Belinski <ebelinski@protonmail.com>
2018-07-12 18:01:51 -0500

    redux boilerplate

---
+++ src/redux.js
@@ -0,0 +1,20 @@
+import emojis from './emojis.json';
+
+export const actionTypes = {
+}
+
+export const initialState = {
+}
+
+export const filterMiddleware = store => next => action => {
+  next(action);
+  switch (action.type) {
+    default: break;
+  }
+}
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: return state;
+  }
+}
