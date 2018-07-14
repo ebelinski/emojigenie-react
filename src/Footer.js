2199e93e4ebfe7e81c972e5ff2afee903197af56
Eugene Belinski <ebelinski@protonmail.com>
2018-07-14 16:51:35 -0500

    button to toggle about

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './Footer.css';
 
 export default class Footer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      aboutIsHidden: true
+    }
+  }
+
   render() {
     return (
       <div className="Footer">
-        Created and maintained by <a href="https://twitter.com/EugeneBelinski">Eugene <span role="img" aria-label="man technologist">👨🏻‍💻</span></a>.
+        <button onClick={() => this.setState({aboutIsHidden: !this.state.aboutIsHidden})}>About</button> | 
+        Created and maintained by <a href="https://twitter.com/EugeneBelinski">Eugene <span role="img" aria-label="man technologist">👨🏻‍💻</span></a>
+
+        {this.state.aboutIsHidden ? null : <div className="about togglable-content">
+          Hello world.
+        </div>}
       </div>
     );
   }
