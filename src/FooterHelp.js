f32dc54c4a4cbf553f0f9e20722acef8473885f9
Eugene Belinski <ebelinski@protonmail.com>
2018-07-14 17:30:52 -0500

    problem not listed question

--- src/FooterHelp.js
+++ src/FooterHelp.js
@@ -17,6 +17,10 @@ export default class FooterHelp extends Component {
         <p class="question">I can't see some of these emojis!</p>
 
         <p>The way emojis are displayed is heavily dependent on your operating system. I designed EmojiGenie work fairly well on macOS. Other operating systems may not have as many emojis.</p>
+        
+        <p class="question">I found a problem not listed here.</p>
+
+        <p>Feel free to <a href="https://github.com/ebelinski/emojigenie-react/issues/new">open an issue</a> in the GitHub repository! Please include what your operating system is, your browser, and how the problem can be replicated.</p>
       </div>
     );
   }
