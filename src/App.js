cd9cafc7ef9e4a00a27192a709974d2803994dff
Eugene Belinski <ebelinski@protonmail.com>
2018-07-14 14:52:35 -0500

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './App.css';
-import { actionTypes } from './redux';
-import copy from 'clipboard-copy';
-import ReactTooltip from 'react-tooltip';
 import Genie from './Genie';
 import Footer from './Footer';
 
