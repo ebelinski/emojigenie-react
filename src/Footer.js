5533eb318116cf872e250f4a270007491b77d71a
Eugene Belinski <ebelinski@protonmail.com>
2018-07-14 15:40:09 -0500

    fix spacing issue for chrome

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,7 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-        Created and maintained by 
-        <a href="https://twitter.com/EugeneBelinski">
-          Eugene <span role="img" aria-label="man technologist">👨🏻‍💻</span>
-        </a>.
+        Created and maintained by <a href="https://twitter.com/EugeneBelinski">Eugene <span role="img" aria-label="man technologist">👨🏻‍💻</span></a>.
       </div>
     );
   }
