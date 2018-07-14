d8cfb4d981105b5ce5d47003ac79e2ca81fec95d
Eugene Belinski <ebelinski@protonmail.com>
2018-07-14 13:02:20 -0500

    linked to myself in footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css';
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        Hello.
+      <div className="Footer">
+        Created and maintained by <a href="https://twitter.com/EugeneBelinski">Eugene 👨🏻‍💻</a>.
       </div>
     );
   }
