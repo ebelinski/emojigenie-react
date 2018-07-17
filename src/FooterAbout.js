99e193b65f40abca485ae28306a71a8c550c50f8
Eugene Belinski <ebelinski@protonmail.com>
2018-07-17 11:36:41 -0500

    added world emoji day

--- src/FooterAbout.js
+++ src/FooterAbout.js
@@ -14,7 +14,7 @@ export default class FooterAbout extends Component {
       <div className="togglable-content">
         <h2>About</h2>
 
-        <p>EmojiGenie was created in July 2018 by <a href="https://ebelinski.com">me</a> because I like emojis, but I always had trouble finding the right one on my computer.</p>
+        <p>EmojiGenie was created in July 2018 by <a href="https://ebelinski.com">me</a> because I like emojis, but I always had trouble finding the right one on my computer. It was launched on the greatest and most important holiday ever envisioned by humanity, World Emoji Day.</p>
       </div>
     );
   }
