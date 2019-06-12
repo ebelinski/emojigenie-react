2cd62580a88e0e3b716be91adad5443529503a92
Eugene Belinski <ebelinski@protonmail.com>
2019-06-12 07:40:59 -0500

    Updates

---
+++ precache-manifest.8723673bfc74564e894d84d1856ea045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ebb1eeb2116df4eae7f0c383b8758d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c20fd29b27372e7a2df3",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b563093e8f008bbb9de8",
+    "url": "/static/css/main.40b839d8.chunk.css"
+  },
+  {
+    "revision": "c20fd29b27372e7a2df3",
+    "url": "/static/js/2.664b2b29.chunk.js"
+  },
+  {
+    "revision": "b563093e8f008bbb9de8",
+    "url": "/static/js/main.d1cbd1e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
